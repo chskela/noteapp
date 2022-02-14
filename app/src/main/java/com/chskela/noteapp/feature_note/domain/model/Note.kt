c057a731b9007894a7c9c287c80ff9cbf9ccaa8a
Chistaykov Aleksey <chskela@gmail.com>
2022-02-14 20:50:53 +0300

    Added Note.kt - Entity for note

---
+++ app/src/main/java/com/chskela/noteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,18 @@
+package com.chskela.noteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.chskela.noteapp.ui.theme.*
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
