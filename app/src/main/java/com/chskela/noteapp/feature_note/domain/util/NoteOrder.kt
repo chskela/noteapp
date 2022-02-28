79263e609b72bd11fe2645e940ac2574341f9041
Chistaykov Aleksey <chskela@gmail.com>
2022-02-28 21:08:55 +0300

    Added OrderSection.kt and update NoteOrder.kt - added fun copy()

--- app/src/main/java/com/chskela/noteapp/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/chskela/noteapp/feature_note/domain/util/NoteOrder.kt
@@ -5,3 +5,11 @@ sealed class NoteOrder(val orderType: OrderType) {
     class Date(orderType: OrderType) : NoteOrder(orderType)
     class Color(orderType: OrderType) : NoteOrder(orderType)
 }
+
+fun NoteOrder.copy(orderType: OrderType) : NoteOrder {
+    return when(this) {
+        is NoteOrder.Title -> NoteOrder.Title(orderType)
+        is NoteOrder.Date -> NoteOrder.Date(orderType)
+        is NoteOrder.Color -> NoteOrder.Color(orderType)
+    }
+}
